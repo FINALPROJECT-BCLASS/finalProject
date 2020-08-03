73008dccb55b350867ac0cbfe49c67ae589316f9
tytnt <tytnt@DESKTOP-ID7501I>
2020-08-03 20:16:33 +0900

    200803/20:15_혜린 : master merge

--- finalProject/src/main/java/com/kh/finalProject/group/controller/GroupController.java
+++ finalProject/src/main/java/com/kh/finalProject/group/controller/GroupController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-
+ 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
