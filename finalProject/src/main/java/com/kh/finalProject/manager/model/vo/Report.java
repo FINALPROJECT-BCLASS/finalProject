145128f5c673cf8c41f72b8d9fcec109fdc77374
dpwl7 <dpwl7@DESKTOP-GBPLJ77>
2020-08-12 21:33:56 +0900

    200812_21:33_yeji report 전면 수정

--- finalProject/src/main/java/com/kh/finalProject/manager/model/vo/Report.java
+++ finalProject/src/main/java/com/kh/finalProject/manager/model/vo/Report.java
@@ -13,27 +13,31 @@ public class Report implements Serializable {
 	private static final long serialVersionUID = -4182975854883157051L;
 	
 	private int rpNo;
-	private String id;
+	private String reportId;
+	private String reportedId;
 	private String rpDate;
 	private String rpCon;
+	private String rpChat;
 	private int rprNo;
 	private String rpRDate;
 	private int rpCount;
 	
 	public Report() {
 	}
-	
-	public Report(int rpNo, String id, String rpDate, String rpCon, int rprNo, String rpResult, String rpRDate,
-			int rpCount) {
+
+	public Report(int rpNo, String reportId, String reportedId, String rpDate, String rpCon, String rpChat, int rprNo,
+			String rpRDate, int rpCount) {
 		this.rpNo = rpNo;
-		this.id = id;
+		this.reportId = reportId;
+		this.reportedId = reportedId;
 		this.rpDate = rpDate;
 		this.rpCon = rpCon;
+		this.rpChat = rpChat;
 		this.rprNo = rprNo;
 		this.rpRDate = rpRDate;
 		this.rpCount = rpCount;
 	}
-	
+
 	public int getRpNo() {
 		return rpNo;
 	}
@@ -42,12 +46,20 @@ public class Report implements Serializable {
 		this.rpNo = rpNo;
 	}
 
-	public String getId() {
-		return id;
+	public String getReportId() {
+		return reportId;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setReportId(String reportId) {
+		this.reportId = reportId;
+	}
+
+	public String getReportedId() {
+		return reportedId;
+	}
+
+	public void setReportedId(String reportedId) {
+		this.reportedId = reportedId;
 	}
 
 	public String getRpDate() {
@@ -64,6 +76,14 @@ public class Report implements Serializable {
 
 	public void setRpCon(String rpCon) {
 		this.rpCon = rpCon;
+	}
+
+	public String getRpChat() {
+		return rpChat;
+	}
+
+	public void setRpChat(String rpChat) {
+		this.rpChat = rpChat;
 	}
 
 	public int getRprNo() {
@@ -92,8 +112,9 @@ public class Report implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Report [rpNo=" + rpNo + ", id=" + id + ", rpDate=" + rpDate + ", rpCon=" + rpCon + ", rprNo=" + rprNo
-				+ ", rpRDate=" + rpRDate + ", rpCount=" + rpCount + "]";
+		return "Report [rpNo=" + rpNo + ", reportId=" + reportId + ", reportedId=" + reportedId + ", rpDate=" + rpDate
+				+ ", rpCon=" + rpCon + ", rpChat=" + rpChat + ", rprNo=" + rprNo + ", rpRDate=" + rpRDate + ", rpCount="
+				+ rpCount + "]";
 	}
 
 }
